d57e120c1f6a6be9ff7f078bdea91eac105b2414
Yaswanthi Unnam <uyaswanthi5962@gmail.com>
2025-03-26 16:19:16 -0500

    Add files via upload

---
+++ includes/includes/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1cardatalake/raw"
+processed_folder_path = "/mnt/formula1cardatalake/processed"
+presentation_folder_path = "/mnt/formula1cardatalake/presentation"
+
+# COMMAND ----------
+
