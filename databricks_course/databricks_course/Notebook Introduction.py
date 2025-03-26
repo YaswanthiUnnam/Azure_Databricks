7b2e1f21ab429884ce9ad5af382f870c81f70372
Yaswanthi Unnam <uyaswanthi5962@gmail.com>
2025-03-26 16:18:44 -0500

    Add files via upload

---
+++ databricks_course/databricks_course/Notebook Introduction.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC create database db;
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
+
+# COMMAND ----------
+
+print("databricks.__version__")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC print("hello")
